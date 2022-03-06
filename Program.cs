7f098fb1e7bdf456341bbd963fb53c9230a4d396
Timi Olaoye <Olaoyeoluwatimileyin@gmail.com>
2022-03-06 15:11:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAppLib;
+
+namespace BankApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            a.CreateAccount();
+           
+            
+            //a.MakeDeposit();
+            
+            //a.MakeWithdrawal();
+            
+        
+        }
+    }
+}
